b6c1905f7f3dca410fa44983525d3786a434fe5e
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-09-10 12:00:32 -0300

    commit main files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
  import React, {Component} from 'react';
 
   const style={
-        fontFamily:'cursive',color:'grey',fontSize:100,
-        textShadow:'2px 1px 4px orange',margin:'auto',textAlign:'center'
+        fontFamily:'cursive',color:'black',fontSize:300,
+        textShadow:'5px 4px 10px grey',margin:'auto',textAlign:'center'
       }
 
-class Salute extends React.Component{
+  class Salute extends React.Component{
     constructor(props){
       super(props); 
       this.state={
-            message:"..."
+           count:0
       }
     }
-  
-       componentDidMount(){
-         setTimeout(()=>{
-           this.setState({
-               message:"Hello, World"
-           })
-         },1400);
-       }
-      
-    
+    reset() {
+      this.setState({
+        count: 0
+      });
+    }
+    increment() {
+      this.setState(state => ({
+        count: this.state.count + 1
+      }));
+    }
+    decrement() {
+      this.setState(state => ({
+        count: this.state.count - 1
+      }));
+    }
     render(){ 
       return(
         <> 
-          <h1 style={style}>{this.state.message}</h1>
+          <button style={{fontSize:23}} className='inc' onClick={this.increment.bind(this)}>Increment</button>
+
+          <button style={{fontSize:23}} className='dec' onClick={this.decrement.bind(this)}>Decrement</button>
+
+          <button style={{fontSize:23}} className='reset' onClick={this.reset.bind(this)}>Reset</button>
+
+          <h1 style={style}> {this.state.count} </h1>
+          
+        
         </>
       );
     };
