d18fd84df3e717b2d9522eec139da95f53bb8b3b
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-09-10 11:45:56 -0300

    Commit main files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ import React, {Component} from 'react';
+
+  const style={
+        fontFamily:'cursive',color:'grey',fontSize:100,
+        textShadow:'2px 1px 4px orange',margin:'auto',textAlign:'center'
+      }
+
+class Salute extends React.Component{
+    constructor(props){
+      super(props); 
+      this.state={
+            message:"..."
+      }
+    }
+  
+       componentDidMount(){
+         setTimeout(()=>{
+           this.setState({
+               message:"Hello, World"
+           })
+         },1400);
+       }
+      
+    
+    render(){ 
+      return(
+        <> 
+          <h1 style={style}>{this.state.message}</h1>
+        </>
+      );
+    };
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Salute />
     </div>
   );
 }
